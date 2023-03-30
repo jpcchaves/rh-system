4abb9c336d7c5f4affc9c2e11dd53fb84010e463
João Paulo <jpcchaves@outlook.com>
2023-03-29 22:41:12 -0300

    feat: add department repository

---
+++ department-service/src/main/java/com/jpcchaves/departmentservice/repository/DepartmentRepository.java
@@ -0,0 +1,9 @@
+package com.jpcchaves.departmentservice.repository;
+
+import com.jpcchaves.departmentservice.entity.Department;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DepartmentRepository extends JpaRepository<Department, Long> {
+}
