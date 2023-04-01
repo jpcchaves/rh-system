d0926148179000bc2b723d0b0ddcb593fdead4fe
João Paulo <jpcchaves@outlook.com>
2023-04-01 11:10:44 -0300

    feat: add apiClient implementation in employee service

--- employee-service/src/main/java/com/jpcchaves/employeeservice/service/impl/EmployeeServiceImpl.java
+++ employee-service/src/main/java/com/jpcchaves/employeeservice/service/impl/EmployeeServiceImpl.java
@@ -1,8 +1,11 @@
 package com.jpcchaves.employeeservice.service.impl;
 
+import com.jpcchaves.employeeservice.dto.APIResponseDto;
+import com.jpcchaves.employeeservice.dto.DepartmentDto;
 import com.jpcchaves.employeeservice.dto.EmployeeDto;
 import com.jpcchaves.employeeservice.entity.Employee;
 import com.jpcchaves.employeeservice.repository.EmployeeRepository;
+import com.jpcchaves.employeeservice.service.APIClient;
 import com.jpcchaves.employeeservice.service.EmployeeService;
 import com.jpcchaves.employeeservice.utils.MapperUtils;
 import org.springframework.stereotype.Service;
@@ -12,10 +15,14 @@ public class EmployeeServiceImpl implements EmployeeService {
 
     private final EmployeeRepository repository;
     private final MapperUtils mapperUtils;
+    private final APIClient apiClient;
 
-    public EmployeeServiceImpl(EmployeeRepository repository, MapperUtils mapperUtils) {
+    public EmployeeServiceImpl(EmployeeRepository repository,
+                               MapperUtils mapperUtils,
+                               APIClient apiClient) {
         this.repository = repository;
         this.mapperUtils = mapperUtils;
+        this.apiClient = apiClient;
     }
 
     @Override
@@ -26,8 +33,14 @@ public class EmployeeServiceImpl implements EmployeeService {
     }
 
     @Override
-    public EmployeeDto getById(Long employeeId) {
+    public APIResponseDto getById(Long employeeId) {
         Employee employee = repository.findById(employeeId).orElseThrow(() -> new RuntimeException("Could not find employee"));
-        return mapperUtils.parseObject(employee, EmployeeDto.class);
+        DepartmentDto departmentDto = apiClient.getDepartment(employee.getDepartmentCode());
+        EmployeeDto employeeDto = mapperUtils.parseObject(employee, EmployeeDto.class);
+
+        return new APIResponseDto(
+                departmentDto,
+                employeeDto
+        );
     }
 }
