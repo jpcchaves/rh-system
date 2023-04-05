5db447e63ef7e9ca25be3cf23272ad367c375043
João Paulo <jpcchaves@outlook.com>
2023-04-05 13:37:24 -0300

    feat: add organization dto

---
+++ organization-service/src/main/java/com/jpcchaves/organizationservice/dto/OrganizationDto.java
@@ -0,0 +1,68 @@
+package com.jpcchaves.organizationservice.dto;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+public class OrganizationDto {
+    private Long id;
+    private String organizationName;
+    private String organizationDescription;
+    private String organizationCode;
+    private LocalDateTime createdAt;
+
+    public OrganizationDto() {
+    }
+
+    public OrganizationDto(Long id, String organizationName, String organizationDescription, String organizationCode, LocalDateTime createdAt) {
+        this.id = id;
+        this.organizationName = organizationName;
+        this.organizationDescription = organizationDescription;
+        this.organizationCode = organizationCode;
+        this.createdAt = createdAt;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getOrganizationName() {
+        return organizationName;
+    }
+
+    public void setOrganizationName(String organizationName) {
+        this.organizationName = organizationName;
+    }
+
+    public String getOrganizationDescription() {
+        return organizationDescription;
+    }
+
+    public void setOrganizationDescription(String organizationDescription) {
+        this.organizationDescription = organizationDescription;
+    }
+
+    public String getOrganizationCode() {
+        return organizationCode;
+    }
+
+    public void setOrganizationCode(String organizationCode) {
+        this.organizationCode = organizationCode;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+}
